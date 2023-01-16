c3eae928cbd2da3b7764e65c9201829b57bd9cf2
AirMouse <airmouselol@gmail.com>
2023-01-16 12:13:14 +0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import App from './App.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<App />
+</div>
+
+<style>
+	.app {
+		min-height: 100vh;
+	}
+</style>
---
+++ src/routes/Scene.svelte
@@ -0,0 +1,90 @@
+<script>
+	import { AmbientLight, DirectionalLight, Mesh } from '@threlte/core';
+	import { AutoColliders, CollisionGroups } from '@threlte/rapier';
+	import * as THREE from 'three';
+	import { BoxBufferGeometry, MeshStandardMaterial } from 'three';
+	import treeImage from '../images/tree.png';
+	import wallImage from '../images/wall.png';
+
+	import Player from './Character.svelte';
+	import Door from './Door.svelte';
+	const loader = new THREE.TextureLoader();
+	const treeGeometry = new THREE.PlaneGeometry(0.5, 2, 5, 5);
+	const treeMaterial = new THREE.MeshBasicMaterial({
+		map: loader.load(treeImage),
+		transparent: true
+	});
+
+	const wallGeometry = new THREE.BoxGeometry(7, 4.55, 0.15);
+	const wallMaterial = new THREE.MeshBasicMaterial({
+		map: loader.load(wallImage),
+		transparent: true
+	});
+</script>
+
+<DirectionalLight shadow position={{ y: 20, x: 8, z: -3 }} />
+<AmbientLight intensity={0.2} />
+
+<CollisionGroups groups={[0, 15]}>
+	<AutoColliders shape={'cuboid'} position={{ y: -0.5 }}>
+		<Mesh
+			receiveShadow
+			geometry={new BoxBufferGeometry(100, 1, 100)}
+			material={new MeshStandardMaterial()}
+		/>
+	</AutoColliders>
+</CollisionGroups>
+
+<CollisionGroups groups={[0]}>
+	<Player position={{ z: 2 }} />
+	<Door />
+
+	<!-- WALLS -->
+	<AutoColliders shape={'cuboid'}>
+		<Mesh
+			receiveShadow
+			castShadow
+			position={{ y: 1.275, x: 30 + 0.7 + 0.15 }}
+			geometry={wallGeometry}
+			material={wallMaterial}
+		/>
+		<Mesh
+			receiveShadow
+			castShadow
+			position={{ y: 1.275, x: 10 + 0.7 + 0.15 }}
+			geometry={wallGeometry}
+			material={wallMaterial}
+		/>
+		<Mesh
+			receiveShadow
+			castShadow
+			position={{ y: 1.275, x: 20 + 0.7 + 0.15 }}
+			geometry={wallGeometry}
+			material={wallMaterial}
+		/>
+		<Mesh
+			receiveShadow
+			castShadow
+			position={{ y: 1.275, x: 30 + 0.7 + 0.15 }}
+			geometry={wallGeometry}
+			material={wallMaterial}
+		/>
+		<Mesh
+			receiveShadow
+			castShadow
+			position={{ y: 1.275, x: 40 + 0.7 + 0.15 }}
+			geometry={wallGeometry}
+			material={wallMaterial}
+		/>
+		<Mesh
+			receiveShadow
+			castShadow
+			position={{ y: 1.275, x: -30 - 0.7 - 0.15 }}
+			geometry={wallGeometry}
+			material={wallMaterial}
+		/>
+
+		<!-- Tree -->
+		<Mesh position={{ y: 1, x: 0, z: 1 }} geometry={treeGeometry} material={treeMaterial} />
+	</AutoColliders>
+</CollisionGroups>

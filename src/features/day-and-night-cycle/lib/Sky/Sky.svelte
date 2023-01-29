7bff6bac197802df638b6259501c68d6c64407d5
MurkyMeow <dinosowermurky@gmail.com>
2023-01-29 04:53:19 +0300

    feature/day-night-cycle: добавляет звуки дня и ночи

--- src/features/day-and-night-cycle/lib/Sky/Sky.svelte
+++ src/features/day-and-night-cycle/lib/Sky/Sky.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import { Three, useFrame } from '@threlte/core';
   import { SkyMesh } from '../SkyMesh';
+  import Sounds from '../Sounds/Sounds.svelte';
 
   let time_sec = 60;
 
   const DAY_LENGTH_SEC = 30;
 
+  $: normalized_time = time_sec / (DAY_LENGTH_SEC * 2);
+
+  $: isDay = Math.sin((Math.PI / 2.0) * Math.cos(normalized_time * Math.PI)) < 0;
+
   const sky = new SkyMesh();
   $: {
-    const normalized_time = time_sec / (DAY_LENGTH_SEC * 2);
-
     sky.update(normalized_time);
   }
 
@@ -19,3 +22,5 @@
 </script>
 
 <Three type={sky} />
+
+<Sounds isDay={isDay} />

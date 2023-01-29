c33333f845bc40a8b9f0bbc73367424856fba393
MurkyMeow <dinosowermurky@gmail.com>
2023-01-29 04:16:47 +0300

    feature/day-night-cycle: устанавливает длину дня в 30 секунд

--- src/features/day-and-night-cycle/lib/Sky/Sky.svelte
+++ src/features/day-and-night-cycle/lib/Sky/Sky.svelte
@@ -2,13 +2,19 @@
   import { Three, useFrame } from '@threlte/core';
   import { SkyMesh } from '../SkyMesh';
 
-  let time = 1;
+  let time_sec = 60;
+
+  const DAY_LENGTH_SEC = 30;
 
   const sky = new SkyMesh();
-  $: sky.update(time);
+  $: {
+    const normalized_time = time_sec / (DAY_LENGTH_SEC * 2);
+
+    sky.update(normalized_time);
+  }
 
   useFrame((_, dt) => {
-    time += dt / 100;
+    time_sec += dt;
   });
 </script>
 

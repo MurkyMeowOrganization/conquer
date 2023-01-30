28641f118aa0be7d374567ac94494735a85767d7
AirMouse <airmouselol@gmail.com>
2023-01-30 17:48:46 +0300

    feature/fireball Добавил фаерболы, нужно заставить их двигаться

--- src/features/fireball/Fireball.svelte
+++ src/features/fireball/Fireball.svelte
@@ -4,15 +4,30 @@
   import { MeshBasicMaterial, TextureLoader } from 'three';
   import lavaImage from '../../images/lavaTexture.png';
 
+  export let isAnimating = false;
+  export const onAnimationEnd = () => {};
+
   const loader = new TextureLoader();
   const lavaTexture = loader.load(lavaImage);
   lavaTexture.magFilter = THREE.NearestFilter;
   const geometry = new THREE.SphereGeometry(0.3, 64, 32);
   const fireballMaterial = new MeshBasicMaterial({ map: lavaTexture });
   let positionZ = 0;
+  const fireballs = [
+    { y: 0.8, x: 0, z: positionZ },
+    { y: 1.5, x: 0, z: positionZ },
+    { y: 2, x: 0, z: positionZ },
+  ];
   useFrame(() => {
-    positionZ -= 0.01;
+    fireballs.map(({ x, y }) => ({ y, x, z: positionZ }));
+
+    if (isAnimating) {
+      console.log('12');
+      positionZ -= 0.04;
+    }
   });
 </script>
 
-<Mesh position={{ y: 0.8, x: 0, z: positionZ }} geometry={geometry} material={fireballMaterial} />
+{#each fireballs as { y, x, z }}
+  <Mesh position={{ y, x, z }} geometry={geometry} material={fireballMaterial} />
+{/each}

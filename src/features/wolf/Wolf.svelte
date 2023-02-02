8c14edde9551e7933626708eb10fb23d3a96c513
AirMouse <airmouselol@gmail.com>
2023-02-02 16:10:36 +0300

    feature/wolf Добавил волка с колайдерами

---
+++ src/features/wolf/Wolf.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Three } from '@threlte/core';
+  import { AutoColliders, RigidBody } from '@threlte/rapier';
+  import { getWolfMesh } from './lib/getWolfMesh';
+
+  const wolfMesh = getWolfMesh();
+</script>
+
+<RigidBody>
+  <AutoColliders shape={'cuboid'}>
+    <Three type={wolfMesh} position={[2, 0.9, -5]} />
+  </AutoColliders>
+</RigidBody>

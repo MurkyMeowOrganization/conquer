81075139d1bdc793567fe8d454cf00e622b02d69
MurkyMeow <dinosowermurky@gmail.com>
2023-02-04 01:49:18 +0300

    feature/terrain-splatting: wip

---
+++ src/features/terrain/Terrain.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Three } from '@threlte/core';
+  import * as THREE from 'three';
+  import { TerrainMesh } from './TerrainMesh';
+
+  const terrainMesh = new TerrainMesh();
+</script>
+
+<Three type={terrainMesh} />
--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -2,6 +2,7 @@
   import { AmbientLight, AudioListener, DirectionalLight, Mesh } from '@threlte/core';
   import { AutoColliders, CollisionGroups } from '@threlte/rapier';
   import * as THREE from 'three';
+  import { Terrain } from 'src/features/terrain';
   import { Sky } from '../features/day-and-night-cycle';
   import treeImage from '../images/tree.png';
   import wallImage from '../images/wall.png';
@@ -44,6 +45,8 @@
     />
   </AutoColliders>
 </CollisionGroups>
+
+<Terrain />
 
 <CollisionGroups groups={[0]}>
   <Door />
